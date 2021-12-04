51b46677939af8ca4a88240964d6fa8ae94c27cf
jkluwa <jkluwa4@gmail.com>
2021-12-04 19:24:04 +0100

    test

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ def create_user(user: UserBase, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_name(db, name=user.name)
     if db_user:
         raise HTTPException(status_code=400, detail="name already used")
-    crud.create_user(db=db, user=user)
-    return signJWT(user.name)
+    signJWT(user.name)
+    return crud.create_user(db=db, user=user)
 
 
 @app.get("/users/", response_model=List[User])
