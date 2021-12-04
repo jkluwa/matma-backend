9d08a58ac303f83bd39f734ca77db0c095e2ac40
jkluwa <jkluwa4@gmail.com>
2021-12-04 15:40:42 +0100

    test

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://sql11456344:V9vKRX4ArL@sql11.freemysqlhosting.net:3306/sql11456344"
-
+SQLALCHEMY_DATABASE_URL = "mysqlx://sql11456344:V9vKRX4ArL@sql11.freemysqlhosting.net:3306/sql11456344"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
