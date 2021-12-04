a553ad019c896549b1f939d660249df57e946621
jkluwa <jkluwa4@gmail.com>
2021-12-04 19:20:22 +0100

    test

--- auth_handler.py
+++ auth_handler.py
@@ -5,8 +5,8 @@ import jwt
 from decouple import config
 
 
-JWT_SECRET = config("secret")
-JWT_ALGORITHM = config("algorithm")
+JWT_SECRET = 'deff1952d59f883eceq9j8abtm9fed21ab0ad9a53323eca4f'
+JWT_ALGORITHM = 'HS256'
 
 
 def token_response(token: str):
