30a9d5c39f74174f1d1ad4d459d7ebd971f2100c
jkluwa <jkluwa4@gmail.com>
2022-01-12 14:55:34 +0100

    matma-backend

--- database.py
+++ database.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://sql11456344:V9vKRX4ArL@sql11.freemysqlhosting.net:3306/sql11456344"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://doadmin:hQfmqZ1kA8PQfqvu@matma-do-user-10522895-0.b.db.ondigitalocean.com:25060/defaultdb"
+ssl_args = {
+    'ssl_ca': 'C:/Users/jkluw/Documents/matma rządzi/backend/ca-certificate.crt'}
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       connect_args=ssl_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
