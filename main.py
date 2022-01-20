fd67c3746a7dd305d0a41df20405f3f863af4f35
jkluwa <jkluwa4@gmail.com>
2022-01-20 19:41:16 +0100

    matma-backend

--- main.py
+++ main.py
@@ -106,6 +106,7 @@ async def listen_to_players(websocket: WebSocket, name: str):
 
 @app.websocket("/active/admin")
 async def check_admin_active(websocket: WebSocket):
+    await websocket.accept()
     while True:
         await websocket.receive_text()
         await websocket.send_text(manager.isAdminActive())
